f3daacbce4c2a6e740bc0cdddc518a2559821f10
TheLime03 <everpadd4@gmail.com>
2023-06-17 18:15:33 +0100

    fixed bug

--- poe_token_check/check.sh
+++ poe_token_check/check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DATE=$(cat date.txt)
-dotnet DiscordChatExporter.Cli.dll export -t $DTOKEN -c $DCHANNEL --after $DATE
+dotnet DiscordChatExporter.Cli.dll export -t $DTOKEN -c $DCHANNEL --after $DATE -f csv -o chat.csv
 NOW=$(date +"%Y-%m-%d")
 echo $NOW > date.txt
