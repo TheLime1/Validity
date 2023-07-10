c79be317aeb77bb7f5a71c4b6c80345f304487f4
TheLime03 <everpadd4@gmail.com>
2023-07-10 19:28:49 +0100

    fix add time to last date (poe) #4

--- poe_token_check/check.sh
+++ poe_token_check/check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DATE=$(cat date.txt)
 dotnet DiscordChatExporter.Cli.dll export -t $DTOKEN -c $DCHANNEL --after $DATE -f csv -o chat.csv
-NOW=$(date +"%Y-%m-%d")
+NOW=$(date +'%d-%b-%y %I:%M %p')
 echo $NOW > date.txt
